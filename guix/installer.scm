c9c45ce12f097ad01b73c0caef4a2c081d828184
YukiEr <charles7714@163.com>
2022-05-30 22:56:27 +0800

    Update installer.scm

--- guix/installer.scm
+++ guix/installer.scm
@@ -38,7 +38,7 @@
 
     ;; Add some extra packages useful for the installation process
     (packages
-     (append (list git curl make emacs-no-x-toolkit)
+     (append (list git curl gnu-make emacs-no-x-toolkit)
              (operating-system-packages installation-os)))))
 
 installation-os-nonfree
